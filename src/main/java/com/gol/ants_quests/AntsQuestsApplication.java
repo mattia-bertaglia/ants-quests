c89e7a3edfdef2712cc6d61713341098b7daf4d1
mattia.bertaglia <mattia.bertaglia@antsrl.com>
2024-06-26 10:45:57 +0200

    Starting project 0.0.1-SNAPSHOT

---
+++ src/main/java/com/gol/ants_quests/AntsQuestsApplication.java
@@ -0,0 +1,13 @@
+package com.gol.ants_quests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntsQuestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntsQuestsApplication.class, args);
+	}
+
+}
