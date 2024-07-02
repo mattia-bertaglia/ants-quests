eef985f0d9aebf4e52a84da7e4214abe28599b7f
CyberZoro23 <raulbrando.salgado@gmail.com>
2024-07-02 12:21:54 +0200

    inserimento script.js
    inserimento script.js che fa la transizione tra login e registrazione

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,17 @@
+// Seleziona i pulsanti e gli elementi necessari
+const signinbtn = document.querySelector('.signinbtn');
+const signupbtn = document.querySelector('.signupbtn');
+const formbox = document.querySelector('.form-box'); // Corretto da .formbox a .form-box
+const body = document.querySelector('body'); // Corretto da .body a body
+
+// Aggiunge un evento di click al pulsante "Registrati"
+signupbtn.onclick = function() {
+  formbox.classList.add('active');
+  body.classList.add('active');
+};
+
+// Aggiunge un evento di click al pulsante "Accedi"
+signinbtn.onclick = function() {
+  formbox.classList.remove('active');
+  body.classList.remove('active');
+};
