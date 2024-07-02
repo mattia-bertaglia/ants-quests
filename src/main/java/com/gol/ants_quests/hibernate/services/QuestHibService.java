685e3113e2146264a52cd3288e16eb2739d29695
Vlad-ino <mistintheforest@outlook.com>
2024-07-02 12:44:09 +0200

    Create varie classi

--- src/main/java/com/gol/ants_quests/hibernate/services/QuestHibService.java
+++ src/main/java/com/gol/ants_quests/hibernate/services/QuestHibService.java
@@ -1,5 +1,21 @@
 package com.gol.ants_quests.hibernate.services;
 
-public class QuestHibService {
+import org.springframework.stereotype.Service;
+import com.gol.ants_quests.hibernate.entities.Quest;
+import com.gol.ants_quests.hibernate.repositories.QuestRepository;
+
+
+
+
+@Service
+public class QuestHibService extends GenericHibService<Quest, Integer, QuestRepository> {
     
+    public QuestHibService(QuestRepository repository) {
+        super(repository);
+    }
+
+
+
+
+
 }
