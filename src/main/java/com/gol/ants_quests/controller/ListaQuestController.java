60c1f9c99cd801bbf509c0badd038a13f9418a6e
Davide-00 <davideboscolo99@gmail.com>
2024-07-02 16:07:54 +0200

    modifica hmtl e controllwe di esiti-questionari

--- src/main/java/com/gol/ants_quests/controller/ListaQuestController.java
+++ src/main/java/com/gol/ants_quests/controller/ListaQuestController.java
@@ -1,13 +1,22 @@
 package com.gol.ants_quests.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.gol.ants_quests.services.ShowQuestServices;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ListaQuestController {
 
-    @GetMapping("/")
-    public String homePage() {
+    private final ShowQuestServices showHibSrv;
+
+    @GetMapping("/esiti")
+    public String esitiQuestionari(Model model) {
+
+        showHibSrv.findAll(model);
+       // model.addAttribute("listaQuestionari", lista);
         return "esitiQuestionari.html";
     }
 }
--- src/main/java/com/gol/ants_quests/services/ShowQuestServices.java
+++ src/main/java/com/gol/ants_quests/services/ShowQuestServices.java
@@ -1,5 +1,20 @@
 package com.gol.ants_quests.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.gol.ants_quests.hibernate.services.EsitiHibService;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ShowQuestServices {
+
+    private final EsitiHibService cliHibSrv;
+
+    public void findAll(Model model) {
+
+        model.addAttribute("listaQuestionari", cliHibSrv.findAll());
+    }
     
 }
