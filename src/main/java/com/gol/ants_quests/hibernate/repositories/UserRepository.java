9c9444380e7e73e9fdfdb3e5610f05834265cbba
CyberZoro23 <raulbrando.salgado@gmail.com>
2024-07-03 14:22:34 +0200

    cambio di role a ruolo in user repository

--- src/main/java/com/gol/ants_quests/hibernate/repositories/UserRepository.java
+++ src/main/java/com/gol/ants_quests/hibernate/repositories/UserRepository.java
@@ -8,18 +8,16 @@ import org.springframework.data.repository.query.Param;
 
 import com.gol.ants_quests.hibernate.entities.User;
 
-
 public interface UserRepository extends JpaRepository<User, Integer> {
     // Metodo per trovare un utente per usernameEmail
     Optional<User> findByUsernameEmail(String usernameEmail);
 
     // Metodo per trovare tutti gli utenti con un determinato ruolo
-    List<User> findByRole(User.Role role);
+    List<User> findByRuolo(User.Ruolo ruolo);
 
     // Metodo per trovare tutti gli utenti abilitati
     List<User> findByEnabled(boolean enabled);
 
-    
-    List<User> findEnableUsersByRole(@Param("role") User.Role role);
-    
+    List<User> findEnableUsersByRuolo(@Param("ruolo") User.Ruolo ruolo);
+
 }
