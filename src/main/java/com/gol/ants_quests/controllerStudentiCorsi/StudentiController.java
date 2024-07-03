48542861c2c8031624dafb88906d8ae262716416
ro-ta <rossellatagliaboschi@libero.it>
2024-07-03 14:22:28 +0200

    in progress
    studenti.html

--- src/main/java/com/gol/ants_quests/controllerStudentiCorsi/StudentiController.java
+++ src/main/java/com/gol/ants_quests/controllerStudentiCorsi/StudentiController.java
@@ -2,6 +2,7 @@ package com.gol.ants_quests.controllerStudentiCorsi;
 
 import org.springframework.stereotype.Controller;
 
+import com.gol.ants_quests.hibernateStudentiCorsi.services.CorsiHibService;
 import com.gol.ants_quests.hibernateStudentiCorsi.services.StudentiHibService;
 
 import lombok.RequiredArgsConstructor;
