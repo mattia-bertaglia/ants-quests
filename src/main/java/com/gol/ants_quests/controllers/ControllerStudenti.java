60d38d8df6e75d12bf9f80c668bce9a34a64bb47
Sam89leu <samueltrecca@gmail.com>
2024-07-03 10:07:33 +0200

    inserimento studente in sessione e invio alla pagina

--- src/main/java/com/gol/ants_quests/controllers/ControllerStudenti.java
+++ src/main/java/com/gol/ants_quests/controllers/ControllerStudenti.java
@@ -2,6 +2,9 @@ package com.gol.ants_quests.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ControllerStudenti {
 
     @GetMapping("/")
-    public String homepageStudente() {
+    public String homepageStudente(HttpSession session) {
+
+        session.setAttribute("studente", stuService.findByUserNameAndPassword);
         return "studenti-home.html";
     }
 
