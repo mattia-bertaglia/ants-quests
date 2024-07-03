5d0c398b5329d21c957bab1d9c3b37c544c52cd5
ro-ta <rossellatagliaboschi@libero.it>
2024-07-03 17:56:37 +0200

    in progress
    css+ html

--- src/main/java/com/gol/ants_quests/controllerStudentiCorsi/StudentiController.java
+++ src/main/java/com/gol/ants_quests/controllerStudentiCorsi/StudentiController.java
@@ -1,6 +1,8 @@
 package com.gol.ants_quests.controllerStudentiCorsi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gol.ants_quests.hibernateStudentiCorsi.services.CorsiHibService;
 import com.gol.ants_quests.hibernateStudentiCorsi.services.StudentiHibService;
@@ -13,5 +15,17 @@ public class StudentiController {
 
     private final StudentiHibService studentiHibService;
     private final CorsiHibService corsiHibService;
+
+    @PostMapping("/studenti")
+    public String studenti(Model model) {
+        model.addAttribute("listaStudenti", studentiHibService.findAll());
+        return "studenti.html";
+    }
+
+    @PostMapping("/corsi")
+    public String corsi(Model model) {
+        model.addAttribute("listaCorsi", corsiHibService.findAll());
+        return "corsi.html";
+    }
 
 }
--- src/main/java/com/gol/ants_quests/hibernateStudentiCorsi/services/CorsiHibService.java
+++ src/main/java/com/gol/ants_quests/hibernateStudentiCorsi/services/CorsiHibService.java
@@ -1,27 +1,30 @@
 package com.gol.ants_quests.hibernateStudentiCorsi.services;
 
 import java.sql.Date;
-
-import javax.xml.crypto.Data;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.gol.ants_quests.hibernateStudentiCorsi.entities.Corso;
 import com.gol.ants_quests.hibernateStudentiCorsi.repositories.CorsiRepository;
 
-import scala.collection.immutable.List;
-
 @Service
-public class CorsiHibService extends GenericHibService <Corso, Integer, CorsiRepository> {
-    public CorsiHibService(CorsiRepository repository){
+public class CorsiHibService extends GenericHibService<Corso, Integer, CorsiRepository> {
+    public CorsiHibService(CorsiRepository repository) {
         super(repository);
     }
-    List<Corso> findByNome(String nome){
+
+    public List<Corso> findByNome(String nome) {
         return getRepository().findByNome(nome);
     }
-    List<Corso> findByDataInzio(Date dataInzio){
+
+    List<Corso> findByDataInzio(Date dataInzio) {
         return getRepository().findByDataInzio(dataInzio);
     }
-    List<Corso> findByDataFine(Date dataFine){
-    return getRepository().findByDataFine(dataFine);
-}
+
+    List<Corso> findByDataFine(Date dataFine) {
+        return getRepository().findByDataFine(dataFine);
+
+    }
+
 }
