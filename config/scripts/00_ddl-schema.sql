91715c6c05c9f56ebd2bef99b33db5046d5f60cc
CyberZoro23 <raulbrando.salgado@gmail.com>
2024-07-03 14:19:31 +0200

    cambio nome da role a ruolo

--- config/scripts/00_ddl-schema.sql
+++ config/scripts/00_ddl-schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE users (
     id_usr INT AUTO_INCREMENT PRIMARY KEY,
     username_email VARCHAR(100) NOT NULL UNIQUE,
     passkey VARCHAR(255) NOT NULL,
-    role ENUM('guest', 'studente', 'admin') DEFAULT 'guest',
-    enable BOOLEAN DEFAULT false
+    ruolo ENUM('guest', 'studente', 'admin') not null DEFAULT 'guest',
+    enabled BOOLEAN DEFAULT false
 );
