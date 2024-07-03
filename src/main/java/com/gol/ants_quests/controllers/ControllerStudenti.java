d0051143d5d012430260d5e07e179753fa887f90
gigio2002 <botrosgiorgio2002@gmail.com>
2024-07-03 14:39:48 +0200

    mod

--- src/main/java/com/gol/ants_quests/controllers/ControllerStudenti.java
+++ src/main/java/com/gol/ants_quests/controllers/ControllerStudenti.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gol.ants_quests.services.QuestsHibService;
-import com.gol.ants_quests.services.StudentsHibService;
+import com.gol.ants_quests.hibernate.services.QuestsHibService;
+import com.gol.ants_quests.hibernate.services.StudentsHibService;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +21,12 @@ public class ControllerStudenti {
     private final StudentsHibService SHS;
 
     @GetMapping("/")
-    public String homepageStudente(HttpSession session,@RequestParam String nome,@RequestParam String cognome) {
+    public String homepageStudente(HttpSession session, @RequestParam String nome, @RequestParam String cognome) {
         // inserire una lista di questionari per lo studente nella session
         session.setAttribute("listaQuestionari", QHS.findAll());
         // lista questionari uguale per ogni studente
         // se assegnato ad una classe vede anche quelli del corso altrimenti no
-        session.setAttribute("studente", SHS.findByNomeAndCognome( nome, cognome));
+        session.setAttribute("studente", SHS.findByNomeAndCognome(nome, cognome));
         return "studenti-home.html";
     }
 
