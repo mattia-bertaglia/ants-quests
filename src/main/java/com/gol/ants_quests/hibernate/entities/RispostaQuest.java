982f4e5da462d43d9427d63c9f973ba6f4d6768a
Vlad-ino <mistintheforest@outlook.com>
2024-07-03 10:28:52 +0200

    Creati classi da capo e sistemato file Studenti

---
+++ src/main/java/com/gol/ants_quests/hibernate/entities/RispostaQuest.java
@@ -0,0 +1,5 @@
+package com.gol.ants_quests.hibernate.entities;
+
+public class RispostaQuest {
+    
+}
---
+++ src/main/java/com/gol/ants_quests/hibernate/entities/Studente.java
@@ -0,0 +1,40 @@
+package com.gol.ants_quests.hibernate.entities;
+
+import java.sql.Date;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "studenti")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Studente extends GenericEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  
+    // private Integer idStudente;
+    private Integer userId; // da eliminare una volta fatta la relazione
+    private String nome;
+    private String cognome;
+    private Date dataNascita;
+    private String cap;
+    private String provincia;
+    private String telefono;
+    private String note;
+    private Date dataInserimento;
+    private Integer corsoId; // da eliminare una volta creata la relazione
+
+    @OneToMany(mappedBy = "studente") 
+    private List<EsitoQuest> esquestionari;
+}
