bde488d05ffddc28b7195108b91cd6fcd1318fdf
Davide-00 <davideboscolo99@gmail.com>
2024-07-03 16:48:08 +0200

    Modifiche Categorie

--- src/main/java/com/gol/ants_quests/controller/GestQuestController.java
+++ src/main/java/com/gol/ants_quests/controller/GestQuestController.java
@@ -1,6 +1,8 @@
 package com.gol.ants_quests.controller;
 
 import com.gol.ants_quests.hibernate.services.CategorieHibService;
+import com.gol.ants_quests.services.GestQuestService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class GestQuestController {
 
-   private final CategorieHibService CatServ;
+   private final GestQuestService CatServ;
    
 
     @GetMapping("/categorie")
     public String categorie(Model model) {
 
-        CatServ.findAll();
+        CatServ.findAll(model);
         return "newquest.html";
     }
        
