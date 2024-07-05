b5a590e2e2fbc71f82173c0350bc42626e9610c0
CyberZoro23 <raulbrando.salgado@gmail.com>
2024-07-05 17:51:38 +0200

    tolto postmapping
    per questo mapping usiamo il get

--- src/main/java/com/gol/ants_quests/controller/AppController.java
+++ src/main/java/com/gol/ants_quests/controller/AppController.java
@@ -3,7 +3,7 @@ package com.gol.ants_quests.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
