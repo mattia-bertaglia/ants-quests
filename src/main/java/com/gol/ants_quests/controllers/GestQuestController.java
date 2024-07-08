eaf1e67347ddf79dc75f2c80bc084eaefc2b5922
Vlad-ino <mistintheforest@outlook.com>
2024-07-08 16:16:39 +0200

    fix

--- src/main/java/com/gol/ants_quests/controllers/GestQuestController.java
+++ src/main/java/com/gol/ants_quests/controllers/GestQuestController.java
@@ -1,20 +1,14 @@
 package com.gol.ants_quests.controllers;
 
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.gol.ants_quests.business.GestQuestService;
-import com.gol.ants_quests.business.QuestService;
-
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -39,7 +33,7 @@ public class GestQuestController {
     public String inserisciQuest(
             @RequestParam String titolo,
             @RequestParam String categoriaId,
-            @RequestBody List<Map<String, Object>> domande) {
+            @RequestBody List<Map<String, Object>> domande) throws Exception {
         gestQuestService.createQuestWithDomandeERisposte(titolo, categoriaId, domande);
         return "redirect:/nuovaQuest";
     }
