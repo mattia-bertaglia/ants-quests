e286568ab6c98bc1ba6e9a33b550d1788d3dcf92
CyberZoro23 <raulbrando.salgado@gmail.com>
2024-07-08 10:14:59 +0200

    fix errorservice

--- src/main/java/com/gol/ants_quests/services/ErrorService.java
+++ src/main/java/com/gol/ants_quests/services/ErrorService.java
@@ -1,35 +1,20 @@
 package com.gol.ants_quests.services;
 
-import java.util.HashMap;
-
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
-import com.gol.ants_quests.error.ErrorsToasts;
-
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
 public class ErrorService {
 
-    private final ErrorsToasts errorList;
-
-    public void getToast(Model model, HashMap<String, String> params) {
-        if (params.containsKey("status")) {
-            String code = params.get("status");
-            model.addAttribute("toastTitle", errorList.get(code).getTitle());
-            model.addAttribute("toastMessage", errorList.get(code).getMessage());
-            model.addAttribute("toastColor", errorList.get(code).getColor());
-        }
+    public void getToast(HttpSession session, String message) {
+        // Implementazione per HttpSession
+        session.setAttribute("toastMessage", message);
     }
 
-    public void getToast(HttpSession session, String code) {
-            session.setAttribute("toastTitle", errorList.get(code).getTitle());
-            session.setAttribute("toastMessage", errorList.get(code).getMessage());
-            session.setAttribute("toastColor", errorList.get(code).getColor());
-        
+    public void getToast(Model model, String message) {
+        // Implementazione per Model
+        model.addAttribute("toastMessage", message);
     }
-
 }
