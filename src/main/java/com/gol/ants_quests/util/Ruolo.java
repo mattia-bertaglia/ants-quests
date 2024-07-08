5b53474517ff2ec8c4294cf11a4fa01d274d973f
mattia.bertaglia <mattia.bertaglia@antsrl.com>
2024-07-08 11:28:37 +0200

    minor fix

--- src/main/java/com/gol/ants_quests/util/Ruolo.java
+++ src/main/java/com/gol/ants_quests/util/Ruolo.java
@@ -4,29 +4,4 @@ public enum Ruolo {
     guest,
     studente,
     admin
-
-    /*
-     * GUEST("guest"),
-     * STUDENTE("studente"),
-     * ADMIN("admin");
-     * 
-     * private String value;
-     * 
-     * Ruolo(String value) {
-     * this.value = value;
-     * }
-     * 
-     * public String getValue() {
-     * return value;
-     * }
-     * 
-     * public static Ruolo fromValue(String value) {
-     * for (Ruolo ruolo : Ruolo.values()) {
-     * if (ruolo.value.equalsIgnoreCase(value)) {
-     * return ruolo;
-     * }
-     * }
-     * throw new IllegalArgumentException("Unknown enum value: " + value);
-     * }
-     */
 }
