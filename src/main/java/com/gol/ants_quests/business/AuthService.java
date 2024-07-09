4793f6ddf4769d360649db47ddff0356035733b6
CyberZoro23 <raulbrando.salgado@gmail.com>
2024-07-09 15:30:14 +0200

    salvataggio del nuovo studente in authservice

--- src/main/java/com/gol/ants_quests/business/AuthService.java
+++ src/main/java/com/gol/ants_quests/business/AuthService.java
@@ -1,5 +1,7 @@
 package com.gol.ants_quests.business;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -7,9 +9,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
-import com.gol.ants_quests.hibernate.entities.OnlyStudente;
+import com.gol.ants_quests.hibernate.entities.Studente;
 import com.gol.ants_quests.hibernate.entities.User;
 import com.gol.ants_quests.hibernate.repositories.UsersRepository;
+import com.gol.ants_quests.hibernate.services.StudentiHibService;
 import com.gol.ants_quests.util.Ruolo;
 
 import jakarta.servlet.http.HttpSession;
@@ -22,6 +25,7 @@ public class AuthService {
     private final BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
 
     private final UsersRepository userRepository;
+    private final StudentiHibService studHibSrv;
     private final ErrorService errorService;
 
     public Optional<User> findByUsernameEmail(String usernameEmail) {
@@ -43,8 +47,28 @@ public class AuthService {
         user.setRuolo(Ruolo.guest);
         user.setFirstTime(false);
 
+        // Salvataggio dell'utente con lo studente temporaneo
+
+        User salvatoUser = userRepository.save(user);
         // popolare dati studente da form di firstTime.html
-        user.setStudente(new OnlyStudente(null, null, null, null, null, null, null, null, null, null, 0));
+        Studente studenteTemp = new Studente(null,
+                salvatoUser,
+                params.get("nome"),
+                params.get("cognome"),
+                Date.valueOf(params.get("dataNascita")),
+                params.get("cap"),
+                params.get("provincia"),
+                params.get("telefono"),
+                params.get("note"),
+                Date.valueOf(LocalDate.now()),
+                null,
+                null);
+        salvatoUser.setStudente(studHibSrv.save(studenteTemp));
+
+        if (salvatoUser == null || salvatoUser.getId() == null) {
+            errorService.getToast(model, "errore di registrazione");
+            return;
+        }
 
         setupSession(session, userRepository.save(user));
     }
