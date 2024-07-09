cb469eecd3e2127bd5b5a74af6a130ce8d7591fb
Emanuela <e.dandria003@gmail.com>
2024-07-09 15:12:59 +0200

    tolto istruzioni js superflue

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -21,20 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var signupForm = document.getElementById('signupForm');
   var emailInput = document.getElementById('mail');
 
-  var emailError = document.getElementById('error-mail');
-  var emailCorrect = document.getElementById('correct-mail');
-
-  emailInput.addEventListener('input', function() {
-      var email = emailInput.value;
-      var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      if (!emailPattern.test(email)) {
-          emailError.classList.remove('hidden');
-          emailCorrect.classList.add('hidden');
-      } else {
-          emailError.classList.add('hidden');
-          emailCorrect.classList.remove('hidden');
-      }
-  });
 
   signupForm.addEventListener('submit', function(event) {
       event.preventDefault(); // Impedisce il submit del form
@@ -48,44 +34,15 @@ document.addEventListener('DOMContentLoaded', function() {
           alert('Devi completare tutti i campi per registrarti.');
           return;
       }
-
-      // Controllo validità email
-      var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      if (!emailPattern.test(email)) {
-          emailError.classList.remove('hidden');
-          return;
-      } else {
-          emailError.classList.add('hidden');
-          emailCorrect.classList.remove('hidden');
-      }
-
-
-      // Se tutti i controlli sono superati, procedere con la registrazione
-      alert('Registrazione avvenuta con successo!');
-      signupForm.submit(); // Invia il form
+      // se tutti i campi sono stati compilati invia il form
+      signupForm.submit();
   });
 });
 
 
 
-// logica per controllare che l'email non sia già esistente all'interno del database
-
-
-
-
-// logica controllo se l'utente è abilitato
-
-
-
 
 // LOGIN
-
-// controllo email
-
-
-
-// controllo password
-
 
 
 
