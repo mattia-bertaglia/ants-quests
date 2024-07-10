c03f906761c7b589987699591b69db6cb55304b0
mattia.bertaglia <mattia.bertaglia@antsrl.com>
2024-07-10 17:58:15 +0200

    minor fix home studenti controller

--- src/main/java/com/gol/ants_quests/controllers/HomeStudentiController.java
+++ src/main/java/com/gol/ants_quests/controllers/HomeStudentiController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gol.ants_quests.business.ExeQuestService;
+import com.gol.ants_quests.business.AuthService;
+import com.gol.ants_quests.business.ErrorService;
+import com.gol.ants_quests.business.HomeStudentiService;
 import com.gol.ants_quests.hibernate.entities.Quest;
 import com.gol.ants_quests.hibernate.entities.User;
 import com.gol.ants_quests.hibernate.services.QuestsHibService;
@@ -27,22 +29,38 @@ public class HomeStudentiController {
 
     private final QuestsHibService QHS;
 
-    private final ExeQuestService questService;
+    private final AuthService authSrv;
+    private final HomeStudentiService questService;
+
+    private final ErrorService errSrv;
 
     @GetMapping("/")
     public String homepageStudente(HttpSession session, Model model) {
         // inserire una lista di questionari per lo studente nella session
         log.info("Apertura Home Page Studente ...");
 
-        User user = (User) session.getAttribute("user");
-        questService.openHomeStud(model, user.getStudente().getIdStudente());
+        // authSrv.checkAuthenticaton(session)
+        if (session.getAttribute("usrlog") != null) {
+            User user = (User) session.getAttribute("user");
+            questService.openHomeStud(model, user.getStudente().getIdStudente());
+
+        } else {
+            log.warn("Home Admin - Non Autorizzato o Sessione Scaduta");
+            errSrv.getToast(session, "notAuthOrOutSession");
+            return "redirect:/";
+        }
 
         log.info("Apertura Home Page Studente ... DONE");
         return "homeStud.html";
     }
 
+    @GetMapping("/profilo")
+    public String openProfilo(HttpSession session, Model model) {
+        return "profiloStud.html";
+    }
+
     @GetMapping("/doQuestionario")
-    public String doQuestionario(HttpSession session, Model model, @RequestParam("mySelect") Long selectedValue) {
+    public String doQuestionario(HttpSession session, Model model, @RequestParam("quest-select") Long selectedValue) {
 
         Optional<Quest> quest = QHS.findByID(selectedValue);
 
