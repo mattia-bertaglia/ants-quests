1345d2cfbf2374747b9ae48ceeee496aa47c0e24
Sam89leu <samueltrecca@gmail.com>
2024-07-11 14:39:04 +0200

    creazione nuovo metodo per fare il conteggio delle domande corrette, creazione metodo per trovare la risposta corretta,

--- src/main/java/com/gol/ants_quests/hibernate/services/EsitiHibService.java
+++ src/main/java/com/gol/ants_quests/hibernate/services/EsitiHibService.java
@@ -23,6 +23,6 @@ public class EsitiHibService extends GenericHibService<EsitoQuest, Long, EsitiQu
     }
 
     public List<EsitoQuest> findByStudente(Long idStudente) {
-        return getRepository().findByStudenteIdStudente(idStudente, Sort.by(Direction.DESC, "dataEsecuzione"));
+        return getRepository().findByStudenteIdStudente(idStudente, Sort.by(Direction.DESC, "idEstQst"));
     }
 }
--- src/main/java/com/gol/ants_quests/hibernate/services/RisposteHibService.java
+++ src/main/java/com/gol/ants_quests/hibernate/services/RisposteHibService.java
@@ -1,5 +1,7 @@
 package com.gol.ants_quests.hibernate.services;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.gol.ants_quests.hibernate.entities.RispostaQuest;
@@ -10,5 +12,11 @@ public class RisposteHibService extends GenericHibService<RispostaQuest, Long, R
 
     public RisposteHibService(RisposteRepository repository) {
         super(repository);
+    }
+
+    
+    public Optional<RispostaQuest> findRispostaCorrettaById(Long idAns) {
+        return getRepository().findRispostaCorrettaById(idAns);
+
     }
 }
