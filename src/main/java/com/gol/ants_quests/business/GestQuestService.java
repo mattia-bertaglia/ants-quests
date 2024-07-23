89ca402e8ec1b51eb8ca4dbf04cb6e9ab23b2e81
mattia.bertaglia <mattia.bertaglia@antsrl.com>
2024-07-23 13:00:04 +0200

    minor fix

--- src/main/java/com/gol/ants_quests/business/GestQuestService.java
+++ src/main/java/com/gol/ants_quests/business/GestQuestService.java
@@ -1,8 +1,10 @@
 package com.gol.ants_quests.business;
 
 import java.util.HashMap;
+
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+
 import com.gol.ants_quests.hibernate.entities.CategoriaQuest;
 import com.gol.ants_quests.hibernate.entities.DomandaQuest;
 import com.gol.ants_quests.hibernate.entities.Quest;
@@ -10,6 +12,7 @@ import com.gol.ants_quests.hibernate.entities.RispostaQuest;
 import com.gol.ants_quests.hibernate.services.DomandeHibService;
 import com.gol.ants_quests.hibernate.services.QuestsHibService;
 import com.gol.ants_quests.hibernate.services.RisposteHibService;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -19,6 +22,10 @@ public class GestQuestService {
     private final QuestsHibService qstSrv;
     private final DomandeHibService domSrv;
     private final RisposteHibService risSrv;
+
+    public void findAll(Model model) {
+        model.addAttribute("listaCategorie", qstSrv.findAll());
+    }
 
     public void findDomandeByID(String domanda_id, Model model) {
         model.addAttribute("quest", qstSrv.findByID(Long.parseLong(domanda_id)).get());
@@ -48,7 +55,7 @@ public class GestQuestService {
             categoria.setIdCat(params.get("type"));
             questEsistente.setCategoriequest(categoria);
             questEsistente.setTitolo(params.get("titolo"));
-            qstSrv.save(questEsistente);    //funzione da cambiare con update
+            qstSrv.save(questEsistente); // funzione da cambiare con update
             id_domanda = params.get("id_quest");
         }
 
@@ -56,18 +63,17 @@ public class GestQuestService {
 
     }
 
-
     public String gestioneDomande(Quest oggetto) {
-       boolean esito = true;
-       boolean modificaRisposte;
-       DomandaQuest domanda = null;
-       
-        for(int i=0;i<oggetto.getDomanda().size();i++){
+        boolean esito = true;
+        boolean modificaRisposte;
+        DomandaQuest domanda = null;
+
+        for (int i = 0; i < oggetto.getDomanda().size(); i++) {
 
             modificaRisposte = true;
-            
-            if(oggetto.getDomanda().get(i).getIdQstDet() == null 
-                && oggetto.getDomanda().get(i).getDomanda() !=null ){
+
+            if (oggetto.getDomanda().get(i).getIdQstDet() == null
+                    && oggetto.getDomanda().get(i).getDomanda() != null) {
 
                 domanda = new DomandaQuest();
                 Quest id_quest = new Quest();
@@ -77,53 +83,52 @@ public class GestQuestService {
                 domSrv.save(domanda);
                 esito = esito && true;
 
-            }else if(oggetto.getDomanda().get(i).getIdQstDet() != null 
-                && oggetto.getDomanda().get(i).getDomanda() == ""){
+            } else if (oggetto.getDomanda().get(i).getIdQstDet() != null
+                    && oggetto.getDomanda().get(i).getDomanda() == "") {
 
                 domSrv.delete(oggetto.getDomanda().get(i).getIdQstDet());
                 esito = esito && true;
                 modificaRisposte = false;
 
-            }else if(oggetto.getDomanda().get(i).getIdQstDet() != null 
-            && oggetto.getDomanda().get(i).getDomanda() != ""){
+            } else if (oggetto.getDomanda().get(i).getIdQstDet() != null
+                    && oggetto.getDomanda().get(i).getDomanda() != "") {
 
                 domanda = domSrv.findByID(oggetto.getDomanda().get(i).getIdQstDet()).get();
                 domanda.setDomanda(oggetto.getDomanda().get(i).getDomanda());
-                domSrv.save(domanda); //da cambiare con update
+                domSrv.save(domanda); // da cambiare con update
                 esito = esito && true;
 
-            }else{
+            } else {
                 modificaRisposte = false;
                 esito = esito && false;
             }
 
+            if (modificaRisposte) {
+                for (int y = 0; y < oggetto.getDomanda().get(i).getRisp().size(); y++) {
 
-            if(modificaRisposte){
-                for(int y=0;y<oggetto.getDomanda().get(i).getRisp().size();y++){
-
-                    if(oggetto.getDomanda().get(i).getRisp().get(y).getIdAns() ==null &&
-                        !oggetto.getDomanda().get(i).getRisp().get(y).getRisposta().equals("")){
+                    if (oggetto.getDomanda().get(i).getRisp().get(y).getIdAns() == null &&
+                            !oggetto.getDomanda().get(i).getRisp().get(y).getRisposta().equals("")) {
                         RispostaQuest risposta = new RispostaQuest();
                         risposta.setRisposta(oggetto.getDomanda().get(i).getRisp().get(y).getRisposta());
                         risposta.setCorretta(oggetto.getDomanda().get(i).getRisp().get(y).getCorretta());
                         risposta.setDomandaQuest(domanda);
                         risSrv.save(risposta);
                         esito = esito && true;
-                    }else if(oggetto.getDomanda().get(i).getRisp().get(y).getIdAns() !=null &&
-                        !oggetto.getDomanda().get(i).getRisp().get(y).getRisposta().equals("")){
-                        //modifica risposta già esistente
-                    }else{
+                    } else if (oggetto.getDomanda().get(i).getRisp().get(y).getIdAns() != null &&
+                            !oggetto.getDomanda().get(i).getRisp().get(y).getRisposta().equals("")) {
+                        // modifica risposta già esistente
+                    } else {
                         esito = esito && false;
                     }
                 }
             }
         }
 
-        if(esito == true){
+        if (esito == true) {
             return "OK";
-        }else{
+        } else {
             return "";
         }
-        
+
     }
 }
