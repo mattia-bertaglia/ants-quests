d65d8579a9b190f455874d10744a982d31fa3079
mattia.bertaglia <mattia.bertaglia@antsrl.com>
2024-07-23 15:27:23 +0200

    fix all structure

---
+++ src/main/resources/static/js/objQuest.js
@@ -0,0 +1,24 @@
+class Quest {
+    constructor() {
+        this.idQst = "";
+        this.titolo = "";
+        this.categoria = { idCat: "", nome: "" }; // Oggetto per categoria
+        this.domanda = []; // ci andranno dentro degli oggetti di tipo Domanda
+    }
+}
+
+class Domanda {
+    constructor() {
+        this.idQstDet = "";
+        this.domanda = "";
+        this.risp = []; // ci andranno dentro degli oggetti di tipo Riposta
+    }
+}
+
+class Risposta {
+    constructor() {
+        this.idAns = "";
+        this.risposta = "";
+        this.corretta = false;
+    }
+}
