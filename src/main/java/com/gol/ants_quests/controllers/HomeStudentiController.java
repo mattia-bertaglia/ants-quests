1bbc5774d550c02ac32e66113729efce39d1f180
Raul-Salgado <raulbrando.salgado@gmail.com>
2024-07-24 15:28:03 +0200

    aggiunti check della sessione + modificaprofilo + apertura profilo

--- src/main/java/com/gol/ants_quests/controllers/HomeStudentiController.java
+++ src/main/java/com/gol/ants_quests/controllers/HomeStudentiController.java
@@ -26,19 +26,15 @@ public class HomeStudentiController {
 
     private final AuthService authSrv;
     private final HomeStudentiService homeStudSrv;
-
     private final ErrorService errorSrv;
 
     @GetMapping("/")
     public String homepageStudente(HttpSession session, Model model) {
-        // inserire una lista di questionari per lo studente nella session
         log.info("Start Open Home Page Studente ...");
 
-        // TODO: authSrv.checkAuthentication(session)
-        if (session.getAttribute("usrlog") != null) {
+        if (authSrv.isLogged(session)) {
             User user = (User) session.getAttribute("user");
             homeStudSrv.openHomeStud(model, user.getStudente().getIdStudente());
-
         } else {
             log.warn("Home Studente - Non Autorizzato o Sessione Scaduta");
             errorSrv.addErrorMessageToSession(session, "notAuthOrOutSession");
@@ -49,51 +45,60 @@ public class HomeStudentiController {
         return "homeStud.html";
     }
 
-    @GetMapping("/profilo")
-    public String openProfilo(HttpSession session, Model model) {
-        // TODO: authSrv.checkAuthentication(session)
-        log.info("Open Pagina Profilo Studente.");
-        // TODO: Compilazione Profilo Utente, aggiungere form per modifica dati.
-        return "profiloStud.html";
-    }
-
-    // TODO: modifica Profilo Studente
-
-    @PostMapping("/modificaProfilo")
-    public String modificaProfilo(HttpSession session, @RequestParam HashMap<String, String> params, Model model) {
-        homeStudSrv.modificaProfilo(session, params, model);
-
-        // Controlla se ci sono errori
-        if (model.containsAttribute("errorMessage")) {
-            return "modificaProfilo"; // Ritorna alla pagina di modifica se ci sono errori
-        }
-
-        // Reindirizza alla pagina del profilo studente se tutto è andato bene
-        return "redirect:/homeStud/profilo";
-    }
-
     @GetMapping("/doQuestionario")
     public String doQuestionario(HttpSession session, Model model, @RequestParam("quest-select") Long selectedValue) {
-        // TODO: authSrv.checkAuthentication(session)
         log.info("Start Questionario=" + selectedValue + " ...");
 
-        homeStudSrv.doQuestionario(model, selectedValue);
-
-        log.info("End Questionario=" + selectedValue);
-        return "doQuest.html";
-
+        if (authSrv.isLogged(session)) {
+            homeStudSrv.doQuestionario(model, selectedValue);
+            log.info("End Questionario=" + selectedValue);
+            return "doQuest.html";
+        } else {
+            log.warn("Do Questionario - Non Autorizzato o Sessione Scaduta");
+            errorSrv.addErrorMessageToSession(session, "notAuthOrOutSession");
+            return "redirect:/";
+        }
     }
 
     @PostMapping("/submit-quest")
     public String submitQuest(HttpSession session, @RequestParam HashMap<String, String> params) {
-        // TODO: authSrv.checkAuthentication(session)
         log.info("Start Submit Questionario ...");
 
-        User user = (User) session.getAttribute("user");
-        homeStudSrv.elaborazioneQuestionario(user, params);
-
-        log.info("End Submit Questionario.");
-        return "redirect:/homeStud/";
+        if (authSrv.isLogged(session)) {
+            User user = (User) session.getAttribute("user");
+            homeStudSrv.elaborazioneQuestionario(user, params);
+            log.info("End Submit Questionario.");
+            return "redirect:/homeStud/";
+        } else {
+            log.warn("Submit Questionario - Non Autorizzato o Sessione Scaduta");
+            errorSrv.addErrorMessageToSession(session, "notAuthOrOutSession");
+            return "redirect:/";
+        }
     }
 
+    @GetMapping("/profilo")
+    public String openProfilo(HttpSession session, Model model) {
+        log.info("Open Pagina Profilo Studente.");
+
+        if (authSrv.isLogged(session)) {
+            return "profiloStud.html";
+        } else {
+            log.warn("View Profilo - Non Autorizzato o Sessione Scaduta");
+            errorSrv.addErrorMessageToSession(session, "notAuthOrOutSession");
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/modificaProfilo")
+    public String modificaProfilo(HttpSession session, @RequestParam HashMap<String, String> params, Model model) {
+        log.info("Dati ricevuti dal form: " + params.toString());
+
+        if (authSrv.isLogged(session)) {
+            authSrv.updateUser(session, params, model);
+            return "redirect:/homeStud/profilo";
+        } else {
+            errorSrv.addErrorMessageToModel(model, "modificaProfiloError");
+            return "redirect:/login";
+        }
+    }
 }
