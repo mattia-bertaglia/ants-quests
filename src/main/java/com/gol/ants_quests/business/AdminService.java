ba2c2fd5e9e1ae0325b3e21463ed3f3b7147006e
Emanuela <e.dandria003@gmail.com>
2024-09-02 18:05:52 +0200

    sistemata visualizzazione dei corsi nel modale dettagli studenti

--- src/main/java/com/gol/ants_quests/business/AdminService.java
+++ src/main/java/com/gol/ants_quests/business/AdminService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.gol.ants_quests.dto.StudenteDTO;
+import com.gol.ants_quests.hibernate.entities.Corso;
 import com.gol.ants_quests.hibernate.entities.Studente;
+import com.gol.ants_quests.hibernate.services.CorsiHibService;
 import com.gol.ants_quests.hibernate.services.EsitiHibService;
 import com.gol.ants_quests.hibernate.services.StudentiHibService;
 
@@ -21,16 +23,19 @@ public class AdminService {
 
     private final StudentiHibService studHibSrv;
     private final EsitiHibService esitiHibSrv;
+    private final CorsiHibService corsiHibSrv;
 
     public void openHomeAdmin(Model model) {
         log.info("Caricamento Ultimi Studenti ...");
         List<Studente> studenti = studHibSrv.findLastStuds();
+        List<Corso> corsi = corsiHibSrv.findAll();
         List<StudenteDTO> result = new ArrayList<StudenteDTO>();
         for (Studente stud : studenti) {
             result.add(new StudenteDTO().convertDto(stud));
         }
 
         model.addAttribute("studenti", result);
+        model.addAttribute("corsi", corsi);
 
         log.info("Caricamento Ultimi Questioanari ...");
         model.addAttribute("listaEsitiQuestionari", esitiHibSrv.findLastEsiti());
