f8e01f7b510b903e14aecaad0328a1014c34f609
ro-ta <rossellatagliaboschi@libero.it>
2024-09-04 15:06:44 +0200

    sessione corsi, stud, admin

--- src/main/java/com/gol/ants_quests/controllers/GesStudentiController.java
+++ src/main/java/com/gol/ants_quests/controllers/GesStudentiController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gol.ants_quests.business.AuthService;
+import com.gol.ants_quests.business.ErrorService;
 import com.gol.ants_quests.business.GesCorsiService;
 import com.gol.ants_quests.business.GesStudentiService;
+import com.gol.ants_quests.util.Ruolo;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
-// Ross & Nass
 @Controller
 @RequestMapping("/ges_studenti")
 @RequiredArgsConstructor
@@ -22,16 +25,41 @@ public class GesStudentiController {
 
     private final GesStudentiService studSrv;
     private final GesCorsiService corsiSrv;
-    /* private final GestQuestService questSrv; */
+    private final AuthService authService;
+    private final ErrorService errorService;
+    private final Ruolo ruolo = Ruolo.admin;
 
     @GetMapping("/")
-    public String getStud(Model model) {
+    public String getStud(HttpSession session, Model model) {
         model.addAttribute("corsi", corsiSrv.findAll());
         model.addAttribute("studenti", studSrv.findAllStudentiDTO());
-        /* questSrv.findAll(model); */
 
-        return "gesStudentiAdmin.html";
+        // Check Autenticazione
+        if (authService.isLogged(session)) {
+
+            return "gesStudentiAdmin.html";
+        } else if (!authService.isLogged(session)) {
+            // Altrimenti manda alla pagina di login con un messaggio di errore
+            errorService.addErrorMessageToSession(session, "notLogged");
+            return "redirect:/";
+        } else if (!authService.hasPermission(session, ruolo)) {
+            errorService.addErrorMessageToSession(session, "noPermission");
+            return "redirect:/";
+        } else {
+            errorService.addErrorMessageToSession(session, "unknownError");
+            return "redirect:/";
+        }
     }
+
+    /*
+     * @GetMapping("/")
+     * public String getStud(Model model) {
+     * model.addAttribute("corsi", corsiSrv.findAll());
+     * model.addAttribute("studenti", studSrv.findAllStudentiDTO());
+     * 
+     * return "gesStudentiAdmin.html";
+     * }
+     */
 
     @PostMapping("/savestud")
     public String saveStud(@RequestParam HashMap<String, String> params) {
